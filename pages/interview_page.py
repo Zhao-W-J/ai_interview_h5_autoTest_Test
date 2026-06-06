d572054380dcd49873b5fce0789ebb2a0b269794
zhao <924961039@qq.com>
2026-06-06 09:49:42 +0800

    修改访问连接

--- pages/interview_page.py
+++ pages/interview_page.py
@@ -6,41 +6,53 @@ class InterviewPage:
     def __init__(self, page):
         self.page = page
 
-    def start_interview(self):
-
-        self.page.get_by_role(
+    async def start_interview(self):
+        """开始面试"""
+        await self.page.get_by_role(
             "button",
             name="我已知晓，开始面试"
         ).click()
 
-        self.page.get_by_role(
+        await self.page.get_by_role(
             "button",
             name="确认开始"
         ).click()
 
-    def answer_question(self):
-
-        self.page.locator(
+    async def answer_question(self):
+        """回答题目"""
+        # 等待按钮出现（增加超时时间）
+        button = self.page.locator(
             "div"
         ).filter(
-            has_text=re.compile(r"^请点击录制开始录制$")
+            has_text=re.compile(r"^点击开始录音|^请点击录制开始录制$")
         ).get_by_role(
             "button"
-        ).click()
+        )
+
+        await button.wait_for(timeout=35000)
+        await button.click()
 
         # 答题录音：30 秒 -10 分钟随机
         random_time = random.randint(30000, 600000)
-        self.page.wait_for_timeout(random_time)
+        await self.page.wait_for_timeout(random_time)
 
-        self.page.locator(
+        # 完成按钮
+        done_button = self.page.locator(
             "div"
         ).filter(
             has_text=re.compile(r"^完成本题$")
         ).get_by_role(
             "button"
-        ).click()
+        )
 
-        self.page.get_by_role(
-            "button",
-            name="确定"
-        ).click()
+        await done_button.wait_for(timeout=10000)
+        await done_button.click()
+
+        # 确认按钮
+        try:
+            await self.page.get_by_role(
+                "button",
+                name="确定"
+            ).click(timeout=5000)
+        except:
+            pass  # 没有确认按钮也继续
